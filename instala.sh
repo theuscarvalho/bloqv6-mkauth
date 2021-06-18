076f57af9287a683e066d3443ce8ee92a73c327d
Matheus Carvalho <33005830+theuscarvalho@users.noreply.github.com>
2021-06-18 07:27:37 -0300

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -5,6 +5,6 @@ wget https://raw.githubusercontent.com/theuscarvalho/bloqv6-mkauth/96413f34377e5
 
 cp /tmp/bloqv6.php /opt/mk-auth/scripts/
 chmod 777 /opt/mk-auth/scripts/bloqv6.php
-echo "*/1 *   * * *   root php /opt/mk-auth/scripts/bloqv6.php 2> /dev/null" > /etc/crontab
+echo "*/1 *   * * *   root php /opt/mk-auth/scripts/bloqv6.php 2> /dev/null" >> /etc/crontab
 
 echo "INSTALADO COM SUCESSO!"
